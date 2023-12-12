c90dd275524bcf7cac68cbe5a6d3467b0929ca44
Divyansh Mishra <divyanshm09@example.com>
2023-12-13 02:01:53 +0530

    yyoo

---
+++ hello.js
@@ -0,0 +1,17 @@
+const user={
+    coursename: "javascript",
+    name:"divynash",
+    yo:"bye",
+    "hehe":"hi",
+    welcomeMessage:function(){
+        console.log(`${this.name},bhai`);
+        console.log(this);
+    }
+}
+
+user.welcomeMessage();
+function hehe()
+{
+    console.log(this);
+}
+// hehe
